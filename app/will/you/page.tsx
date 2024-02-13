0e78320aad4e51aaa637c470709811440c79ddcf
yudiptr <yudiputrasabri789@gmail.com>
2024-02-13 21:37:57 +0700

    hehe

--- app/will/you/page.tsx
+++ app/will/you/page.tsx
@@ -2,6 +2,7 @@ import { BackgroundGradientAnimation } from "@/components/ui/background-gradient
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import { dancing_script } from "@/lib/fonts";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 export default function Home() {
   const line1 = `"Thanks for all the things u been thru w 'us', hopin more more days with u,My dear, my valentine's true."`;
   return (
@@ -13,11 +14,61 @@ export default function Home() {
             className={`text-2xl md:text-3xl lg:text-8xl ${dancing_script.className}`}
           />
           <div className="flex justify-center">
+            
             <Image
-              src={"/lala.jpg"}
-              alt="lala"
-              width={400}
-              height={300}/>
+              src={"https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"}
+              alt="puki"
+              width={200}
+              height={200}/>
+              
+          </div>
+          <div className="flex justify-center gap-3">
+            <Marquee className="flex justify-center gap-3">
+              <div className="flex justify-center gap-5">
+                  <Image
+                    className="rounded-xl border-2"
+                    src={"/lala.jpg"}
+                    alt="lala"
+                    width={200}
+                    height={100}
+                  />
+                  <Image
+                    className="rounded-xl border-2"
+                    src={"/lala1.jpg"}
+                    alt="lala"
+                    width={200}
+                    height={100}
+                  />
+                  <Image
+                    className="rounded-xl border-2"
+                    src={"/lala2.jpg"}
+                    alt="lala"
+                    width={200}
+                    height={100}
+                  />
+                  <Image
+                    className="rounded-xl border-2"
+                    src={"/lala3.jpg"}
+                    alt="lala"
+                    width={200}
+                    height={100}
+                  />
+                  <Image
+                    className="rounded-xl border-2"
+                    src={"/lala4.jpg"}
+                    alt="lala"
+                    width={200}
+                    height={100}
+                  />
+                  <Image
+                    className="rounded-xl border-2"
+                    src={"/lala5.jpg"}
+                    alt="lala"
+                    width={200}
+                    height={100}
+                  />
+              </div>
+            </Marquee>
           </div>
           <TextGenerateEffect
             words={"love u lala jelek"}
